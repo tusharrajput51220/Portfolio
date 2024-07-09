ef45b3d1e137d866a0a78ce72213f9121e731527
Tushar <142966652+tusharrajput51220@users.noreply.github.com>
2024-07-09 06:21:01 +0000

    bb

--- my-next-app/app/components/Header.tsx
+++ my-next-app/app/components/Header.tsx
@@ -1,22 +1,55 @@
-import React from 'react'
-import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion';
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
-type Props = {}
-function Header({ }: Props) {
-    return (
-        <header className='sticky top-0 flex items-start p-5 justify-between max-w-5xl mx-auto z-20 xl:items-center'>
-            <div>
-                {/* Social apps */}
-                <SocialIcon url="https://instagram.com/choco_51220" fgColor='gray' bgColor='transparent' />
-                <SocialIcon url="https://linkedin.com/in/tushar-rajput-420710251" fgColor='gray' bgColor='transparent' />
-                <SocialIcon url="https://github.com/tusharrajput51220" fgColor='gray' bgColor='transparent' />
-            </div>
-            <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
-                <SocialIcon className='cursor-pointer' network='email' fgColor='gray' bgColor='transparent' />
-                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
-            </div>
-        </header>
-    )
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header className='sticky top-0 flex items-start p-5 justify-between max-w-5xl mx-auto z-20 xl:items-center'>
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        className='flex flex-row items-center'
+      >
+        {/* Social apps */}
+        <SocialIcon
+          url='https://instagram.com/choco_51220'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <SocialIcon
+          url='https://linkedin.com/in/tushar-rajput-420710251'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <SocialIcon
+          url='https://github.com/tusharrajput51220'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+      </motion.div>
+      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        <SocialIcon
+          className='cursor-pointer'
+          network='email'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+          Get in touch
+        </p>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
