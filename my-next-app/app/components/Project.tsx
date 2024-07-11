4fe00dfb14098dffff2a8a6f896a581537b935ff
Tushar <142966652+tusharrajput51220@users.noreply.github.com>
2024-07-11 08:00:41 +0000

    bbbb

--- my-next-app/app/components/Project.tsx
+++ my-next-app/app/components/Project.tsx
@@ -15,31 +15,32 @@ function Project({ }: Props) {
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20
        scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
-      <Projects src="n2.png" heading='netflix clone' subject='Developed a Netflix clone, a web
+        <Projects src="n2.png" heading='netflix clone' subject='Developed a Netflix clone, a web
        application mimicking the popular streaming platform. It features a sleek, user-friendly 
        interface with categories, search functionality, and responsive design. Users can browse a 
        curated collection of movies and TV shows, view details, and stream content seamlessly. The 
        project leverages modern web technologies such as React for the frontend and a robust backend
-        to manage content and user data, ensuring a smooth and engaging viewing experience.' />
+        to manage content and user data, ensuring a smooth and engaging viewing experience.'
+          projectlink='https://github.com/tusharrajput51220/Netflix-clone' />
 
-      <Projects src="google.png" heading='Google realtime' subject='I developed a Google clone utilizing 
+        <Projects src="google.png" heading='Google realtime' subject='I developed a Google clone utilizing 
       React for the frontend and Node.js for the backend. The project integrates Google APIs to mimic 
       core functionalities, providing users with a familiar search experience. React component-based
        architecture ensures a responsive and dynamic interface, while Node.js handles server-side 
        operations and API interactions efficiently. This clone showcases my skills in modern web
         development technologies and demonstrates my ability to replicate complex web services with
-         practical implementation.
-         .' />
+         practical implementation.' projectlink='https://github.com/tusharrajput51220/Chat-app/tree/master' />
 
       <Projects src="chatapp.png" heading='realtime chatapp' subject='Created a real-time chat application 
       leveraging React for the user interface, Firebase and Firestore for real-time data synchronization,
        and Node.js for backend logic. This setup ensures instant messaging and seamless updates across
         all clients. React dynamic components provide a smooth user experience, while Firebase and 
         Firestore manage and sync chat data in real time. Node.js supports the application’s backend 
-        processes, ensuring robust performance and scalability for the chat service.' />
-        </div>
+        processes, ensuring robust performance and scalability for the chat service.'
+          projectlink='https://github.com/tusharrajput51220/Chat-app/tree/master' />
+      </div>
 
-      
+
       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"></div>
     </div>
   );
--- my-next-app/app/components/Projects.tsx
+++ my-next-app/app/components/Projects.tsx
@@ -4,9 +4,10 @@ type Props = {
     src: string,
     heading: string,
     subject: string,
+    projectlink: string,
 };
 
-function Projects({ src, heading, subject }: Props) {
+function Projects({ src, heading, subject,projectlink }: Props) {
     return (
     //     <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20
     //    scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 ">
@@ -16,7 +17,7 @@ function Projects({ src, heading, subject }: Props) {
                 <img src={src} alt="" className="w-[120px] h-[120px] rounded" />
                 <div className="space-y-10 px-0 md:px-10 max-w-6xl">
                     <h4 className="text-4xl font-semibold text-center">
-                        <a className="underline uppercase decoration-[#F7AB0A]/50" href="https://github.com/tusharrajput51220/Netflix-clone">
+                        <a className="underline uppercase decoration-[#F7AB0A]/50" href={projectlink}>
                             {heading}
                         </a>
                     </h4>
