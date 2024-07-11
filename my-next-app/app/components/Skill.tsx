d1ece8fb04a80705fbe4a587d591d64db2ca36de
Tushar <142966652+tusharrajput51220@users.noreply.github.com>
2024-07-11 07:00:16 +0000

    ghh

--- my-next-app/app/components/Skill.tsx
+++ my-next-app/app/components/Skill.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  src: string,
+  number: string,
+};
 
-function Skill({}: Props) {
+function Skill({src,number}: Props) {
   return (
     <div className="group flex relative cursor-pointer">
       <img
-        src="reac.jpg"
-        alt=""
+        src={src}
+        alt=''
         className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-[80px] xl:h-[80px] filter 
         group-hover:grayscale transition duration-300 ease-in-out"
       />
@@ -17,7 +20,7 @@ function Skill({}: Props) {
       h-24 w-24 md:w-28 md:h-28 xl:w-[80px] xl:h-[80px] rounded-full z-0"
       >
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+          <p className="text-3xl font-bold text-black opacity-100">{number}</p>
         </div>
       </div>
     </div>
--- my-next-app/app/components/Skills.tsx
+++ my-next-app/app/components/Skills.tsx
@@ -16,21 +16,23 @@ function Skills({}: Props) {
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
         Hover over a skill for current proficiency
       </h3>
-      <div className="grid grid-cols-4 gap-5 relative top-[50px]"> 
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+      <div className="grid grid-cols-4 gap-5 relative top-[70px]"> 
+        <Skill src="html.jpg" number='98%' />
+        <Skill src="csss.jpg" number='99%'/>
+        <Skill src="tailwind.png" number='95%'/>
+        <Skill src="boot.jpg" number='80%'/>
+        <Skill src="jpt.jpg" number='95%'/>
+        <Skill src="reac.jpg" number='97%'/>
+        <Skill src="next.jpg" number='91%'/>
+        <Skill src="ts.jpg" number='80%'/>
+        <Skill src="node.jpg" number='85%'/>
+        <Skill src="express.png" number='78%'/>
+        <Skill src="java.png" number='82%'/>
+        <Skill src="cpp.jpg" number='78%'/>
+        <Skill src="fire.png" number='80%'/>
+        <Skill src="mongo.png" number='75%'/>
+        <Skill src="mysql.png" number='90%'/>
+        <Skill src="github.jpg" number='89%'/>
       </div>
     </div>
   );
