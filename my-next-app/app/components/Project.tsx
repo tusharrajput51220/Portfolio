2e746b1f1693dd88516957d9d4260951e8686a47
Tushar <142966652+tusharrajput51220@users.noreply.github.com>
2024-07-11 07:30:49 +0000

    ddd

--- my-next-app/app/components/Project.tsx
+++ my-next-app/app/components/Project.tsx
@@ -1,9 +1,9 @@
 import React from "react";
+import Projects from "./Projects";
 
 type Props = {};
 
-function Project({}: Props) {
-  const projects = [1, 2, 3, 4, 5];
+function Project({ }: Props) {
   return (
     <div
       className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly 
@@ -12,35 +12,34 @@ function Project({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
+
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20
-       scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 ">
-        {projects?.map((project, i) => (
-          <div
-            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20
-            md:p-44 h-screen"
-          >
-            <img src="n2.png" alt="" className="w-[120px] h-[120px] rounded" />
-            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
-              <h4 className="text-4xl font-semibold text-center">
-                <span className="underline  decoration-[#F7AB0A]/50">
-                  Case Study {i + 1} of {projects?.length}:
-                </span>
-                UPS Clone
-              </h4>
-              <p className="text-lg text-center md:text-left">
-                I developed a Netflix clone, a web application mimicking the
-                popular streaming platform. It features a sleek, user-friendly
-                interface with categories, search functionality, and responsive
-                design. Users can browse a curated collection of movies and TV
-                shows, view details, and stream content seamlessly. The project
-                leverages modern web technologies such as React for the frontend
-                and a robust backend to manage content and user data, ensuring a
-                smooth and engaging viewing experience.
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
+       scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <Projects src="n2.png" heading='netflix clone' subject='Developed a Netflix clone, a web
+       application mimicking the popular streaming platform. It features a sleek, user-friendly 
+       interface with categories, search functionality, and responsive design. Users can browse a 
+       curated collection of movies and TV shows, view details, and stream content seamlessly. The 
+       project leverages modern web technologies such as React for the frontend and a robust backend
+        to manage content and user data, ensuring a smooth and engaging viewing experience.' />
+
+      <Projects src="google.png" heading='Google realtime' subject='I developed a Google clone utilizing 
+      React for the frontend and Node.js for the backend. The project integrates Google APIs to mimic 
+      core functionalities, providing users with a familiar search experience. React component-based
+       architecture ensures a responsive and dynamic interface, while Node.js handles server-side 
+       operations and API interactions efficiently. This clone showcases my skills in modern web
+        development technologies and demonstrates my ability to replicate complex web services with
+         practical implementation.
+         .' />
+
+      <Projects src="chatapp.png" heading='netflix clone' subject='Created a real-time chat application 
+      leveraging React for the user interface, Firebase and Firestore for real-time data synchronization,
+       and Node.js for backend logic. This setup ensures instant messaging and seamless updates across
+        all clients. React dynamic components provide a smooth user experience, while Firebase and 
+        Firestore manage and sync chat data in real time. Node.js supports the application’s backend 
+        processes, ensuring robust performance and scalability for the chat service.' />
+        </div>
+
+      
       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"></div>
     </div>
   );
---
+++ my-next-app/app/components/Projects.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+type Props = {
+    src: string,
+    heading: string,
+    subject: string,
+};
+
+function Projects({ src, heading, subject }: Props) {
+    return (
+    //     <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20
+    //    scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 ">
+            <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20
+            md:p-44 h-screen"
+            >
+                <img src={src} alt="" className="w-[120px] h-[120px] rounded" />
+                <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+                    <h4 className="text-4xl font-semibold text-center">
+                        <a className="underline uppercase decoration-[#F7AB0A]/50" href="https://github.com/tusharrajput51220/Netflix-clone">
+                            {heading}
+                        </a>
+                    </h4>
+                    <p className="text-lg text-center md:text-left">
+                        {subject}
+                    </p>
+                </div>
+            </div>
+        // </div>
+    );
+}
+
+export default Projects;
